943a550d05d1d898ad854034600aa30ec3ac21de
ballack <rodrigue-dev>
2023-06-26 11:32:39 +0100

    azer

--- resources/views/front/base.blade.php
+++ resources/views/front/base.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <meta name="cryptomus" content="3f159f83" />
     <title>{{env('APP_NAME')}} | </title>
     <!-- Icon Font Stylesheet -->
     <link href="{{ asset('multi/css/all.min.css') }}" rel="stylesheet">
