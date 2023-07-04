27cf2c80f5d7581abfad26e5f542eae05045b099
ballack <rodrigue-dev>
2023-07-04 17:24:59 +0100

    azer

--- app/Http/Controllers/Front/FrontController.php
+++ app/Http/Controllers/Front/FrontController.php
@@ -235,7 +235,7 @@ class FrontController extends Controller
             if ($request->get('payement_method')=='cinetpay'){
               $response = $this->cinetPayService->sendPayment([
                     'transaction_id' => $orderkey,
-                    "amount" =>round($total * helpers::setPrice(\session()->get('currency')),2),
+                    "amount" =>round($total * helpers::setPrice(\session()->get('currency'))),
                     'currency' => \session()->get('currency'),
                     'description' => "Paiement",
                     'customer_name' => $customer->name,
